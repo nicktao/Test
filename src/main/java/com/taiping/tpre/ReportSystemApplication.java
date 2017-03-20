a6cd509bbd3689b56ae2bfbaf5131104dd0930a6
nick <nicktaochun@gmail.com>
2017-03-20 14:10:05 +0800

    initial

---
+++ src/main/java/com/taiping/tpre/ReportSystemApplication.java
@@ -0,0 +1,12 @@
+package com.taiping.tpre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportSystemApplication.class, args);
+	}
+}
